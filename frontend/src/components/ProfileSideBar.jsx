4a9533fc8e298a9d103b6720dbe1b0ef722afc91
biruk1122 <birukfikadu29@gmail.com>
2024-07-16 02:38:25 -0700

    complete the UI functionality of the dashboard for profile and signout

---
+++ frontend/src/components/ProfileSideBar.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react"
+import { Sidebar } from "flowbite-react"
+import { Link, useLocation } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { HiUser, HiArrowSmRight } from "react-icons/hi" // Assuming you are using react-icons
+import { logout } from "./path-to-logout-action" // Adjust the path to your logout action
+
+export default function ProfileSideBar() {
+  const { currentUser } = useSelector((state) => state.user)
+  const destination = useLocation()
+  const [tab, setTab] = useState("")
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const urlParms = new URLSearchParams(destination.search)
+    const tabFromUrl = urlParms.get("tab")
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [destination.search])
+
+  const logoutUser = () => {
+    dispatch(logout())
+  }
+
+  return (
+    <Sidebar className="w-full md:w-60">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className="flex flex-col gap-2">
+          <Sidebar.Item
+            as={Link}
+            to="/dashboard?tab=profile"
+            active={tab === "profile"}
+            icon={HiUser}
+            label={currentUser.administrator ? "Admin" : "User"}
+            labelColor="dark"
+          >
+            Profile
+          </Sidebar.Item>
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={logoutUser}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import ProfileSideBar from "../components/ProfileSideBar"
+import Profile from "../components/Profile"
 
 export default function Dashboard() {
-  return <div>Dashboard</div>
+  const destination = useLocation()
+  const [tab, setTab] = useState("")
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(destination.search)
+    const tabFromUrl = urlParams.get("tab")
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [destination.search])
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-60">
+        {/* Sidebar */}
+        <ProfileSideBar />
+      </div>
+      {/* Profile  */}
+      {tab === "profile" && <Profile />}
+    </div>
+  )
 }
