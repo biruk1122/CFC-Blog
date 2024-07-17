3bc43003d540191a6bd99aafff734551f4232545
biruk1122 <birukfikadu29@gmail.com>
2024-07-17 08:05:23 -0700

    complete generate post functionality

--- frontend/src/pages/AddPost.jsx
+++ frontend/src/pages/AddPost.jsx
@@ -3,32 +3,77 @@ import "react-quill/dist/quill.snow.css"
 
 
 
-const [imageFile, setImageFile] = useState(null)
-  const [uploadProgress, setUploadProgress] = useState(null)
-
-
-  const imageUploadProcessor = async () => {
-    try {
-      if (!imageFile) {
-        setUploadError("Select an image please.")
-        return
+export default function AddPost() {
+    const [imageFile, setImageFile] = useState(null)
+    const [uploadProgress, setUploadProgress] = useState(null)
+    const [uploadError, setUploadError] = useState(null)
+    const [dataForm, setDataForm] = useState({})
+    const redirect = useNavigate()
+    const [submissionFailure, setSubmissionFailure] = useState(null)
+  
+    const imageUploadProcessor = async () => {
+      try {
+        if (!imageFile) {
+          setUploadError("Select an image please.")
+          return
+        }
+        setUploadError(null)
+  
+        const storage = getStorage(app)
+        const imageName = new Date().getTime() + "-" + imageFile.name
+        const storageRef = ref(storage, imageName)
+        const handleTaskUpload = uploadBytesResumable(storageRef, imageFile)
+  
+        handleTaskUpload.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            setUploadProgress(progress.toFixed(0))
+          },
+          (error) => {
+            setUploadError("Image upload failed")
+            setUploadProgress(null)
+          },
+          () => {
+            getDownloadURL(handleTaskUpload.snapshot.ref).then((downloadURL) => {
+              setUploadProgress(null)
+              setUploadError(null)
+              setDataForm({ ...dataForm, image: downloadURL })
+            })
+          }
+        )
+      } catch (error) {
+        setUploadError("Image upload failed")
+        setUploadProgress(null)
+        console.log(error)
       }
-      setUploadError(null)
+    }
+    const submitAction = async (e) => {
+      e.preventDefault()
+      try {
+        const res = await fetch("/api/post/generate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataForm),
+        })
+        const details = await res.json()
+        if (!res.ok) {
+          setSubmissionFailure(details.message)
+          return
+        }
+        if (res.ok) {
+          setSubmissionFailure(null)
+          redirect(`/post/${details.permalink}`)
+        }
+      } catch (error) {
+        setSubmissionFailure("An error has occurred.")
+      }
+    }
 
-      const storage = getStorage(app)
-      const imageName = new Date().getTime() + "-" + imageFile.name
-      const storageRef = ref(storage, imageName)
-      const handleTaskUpload = uploadBytesResumable(storageRef, imageFile)
-
-      handleTaskUpload.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          setUploadProgress(progress.toFixed(0))
-        },
-
-
+}
 
 
 export default function AddPost() {
@@ -46,4 +91,5 @@ return (
             onChange={(e) =>
               setDataForm({ ...dataForm, subject: e.target.value })
             }
+        )
         }
