8601a7d6386900ae4021c4e25f4ee0cb327283ac
biruk1122 <birukfikadu29@gmail.com>
2024-07-17 07:37:11 -0700

    add upload image post functionality

--- frontend/src/pages/AddPost.jsx
+++ frontend/src/pages/AddPost.jsx
@@ -1,3 +1,36 @@
+import ReactQuill from "react-quill"
+import "react-quill/dist/quill.snow.css"
+
+
+
+const [imageFile, setImageFile] = useState(null)
+  const [uploadProgress, setUploadProgress] = useState(null)
+
+
+  const imageUploadProcessor = async () => {
+    try {
+      if (!imageFile) {
+        setUploadError("Select an image please.")
+        return
+      }
+      setUploadError(null)
+
+      const storage = getStorage(app)
+      const imageName = new Date().getTime() + "-" + imageFile.name
+      const storageRef = ref(storage, imageName)
+      const handleTaskUpload = uploadBytesResumable(storageRef, imageFile)
+
+      handleTaskUpload.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          setUploadProgress(progress.toFixed(0))
+        },
+
+
+
+
 export default function AddPost() {
 return (
     <div className="p-4 max-w-3xl mx-auto min-h-screen">
