2a7d56f648a4713b2002b9baa9b090761d9a4d8f
biruk1122 <birukfikadu29@gmail.com>
2024-07-17 05:31:06 -0700

    adding user image upload functionality

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,5 +1,59 @@
+import { Alert, Button, Modal, TextInput } from "flowbite-react"
+import React, { useEffect, useRef, useState } from "react"
+
 export default function Profile() {
     return (
+        const { currentUser, error } = useSelector((state) => state.user)
+        const [image, setImage] = useState(null)
+        const [imageUrl, setImageUrl] = useState(null)
+        const [uploadProgress, setUploadProgress] = useState(null)
+
+        const onImageChange = (e) => {
+            const file = e.target.files[0]
+            if (file) {
+              setImage(file)
+              setImageUrl(URL.createObjectURL(file))
+            }
+          }
+          useEffect(() => {
+            if (image) {
+              uploadFile()
+            }
+          }, [image])
+
+          const uploadFile = async () => {
+            setFileUpload(true)
+            setUploadError(null)
+            const storage = getStorage(app)
+            const fileName = new Date().getTime() + image.name
+        
+            const storageRef = ref(storage, fileName)
+            const fileUploadTask = uploadBytesResumable(storageRef, image)
+        
+            fileUploadTask.on(
+              "state_changed",
+              (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                setUploadProgress(progress.toFixed(0))
+              },
+              (error) => {
+                setUploadError("File must be less than 3MB")
+        
+                setUploadProgress(null)
+                setImage(null)
+                setImageUrl(null)
+                setFileUpload(false)
+              },
+              () => {
+                getDownloadURL(fileUploadTask.snapshot.ref).then((downloadURL) => {
+                  setImage(downloadURL)
+                  setFormData({ ...formData, avatar: downloadURL })
+                  setFileUpload(false)
+                })
+              }
+            )
+          }
+
         <div className="max-w-lg mx-auto p-4 w-full">
           <h1 className="my-5 text-center font-semibold text-3xl">Profile</h1>
           <form onSubmit={handleSubmit} className="flex flex-col gap-5">
