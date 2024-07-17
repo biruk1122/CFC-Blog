a5689b8d0b6c7b5b461c33ffa35d3ea854c4235f
biruk1122 <birukfikadu29@gmail.com>
2024-07-17 06:25:13 -0700

    adding user model adminstrator functionality

--- backend/model/user_model.js
+++ backend/model/user_model.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema(
       default:
         "https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg",
     },
+    administrator: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true } // Enable timestamps for createdAt and updatedAt fields
 )
