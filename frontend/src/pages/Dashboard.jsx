0eae5fa326b6108f4d7b4438ab2142cd43f04ed8
biruk1122 <birukfikadu29@gmail.com>
2024-07-21 08:01:32 -0700

    compliting the dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import ProfileSideBar from "../components/ProfileSideBar"
 import Profile from "../components/Profile"
+import PostPages from "../components/PostPages"
+import MembersDashBoard from "../components/MembersDashBoard"
+import CommentDashBoard from "../components/CommentDashBoard"
+import DashBoardComponent from "../components/DashBoardComponent"
 
 export default function Dashboard() {
   const destination = useLocation()
@@ -25,6 +29,12 @@ export default function Dashboard() {
       {tab === "profile" && <Profile />}
       {/*post*/}
       {tab === "posts" && <PostPages />}
+      {/*users */}
+      {tab === "members" && <MembersDashBoard />}
+      {/* Comments */}
+      {tab === "comments" && <CommentDashBoard />}
+      {/* DashBoardComponent */}
+      {tab === "dash" && <DashBoardComponent />}
     </div>
   )
 }
